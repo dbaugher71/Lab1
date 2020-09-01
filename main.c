d6f57c1c63381da6514bd50fee20cbf39409141a
dbaugher71 <dvb5709@psu.edu>
2020-09-01 19:22:07 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// Collaborator: Mackenzie DeArment mqd5802@psu.edu
+// Collaborator: Jingsheng Zang zjs899171@gmail.com
+// COllaborator: Ingi Hong iqh5105@psu.edu
+
+
+
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
